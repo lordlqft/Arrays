7f66620a8bddc62f2ae5410d35bcf23413baa696
Kenneth <lordlqft0@gmail.com>
2026-05-19 16:16:03 -0300

    Feat: Atividade6
    Exibindo lista completa dos nomes dentro de um array

---
+++ Atividade6.cs
@@ -0,0 +1,17 @@
+/*
+string[] nomeAlunos = { };
+
+while (true)
+{
+    Console.Write("Digite o nome do aluno (ou 'sair' para encerrar): ");
+    string nome = Console.ReadLine()!;
+
+    if (nome.ToLower() == "sair")
+    {
+        break;
+    }
+
+    nomeAlunos = nomeAlunos.Append(nome).ToArray();
+    Console.WriteLine($"Nomes dos alunos: {string.Join(", ", nomeAlunos)}");
+}
+*/
