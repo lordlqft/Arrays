e77f881c36aeebb5e89ef44712b420cdf048efd3
Kenneth <lordlqft0@gmail.com>
2026-05-19 16:15:28 -0300

    Feat: Atividade5
    Exibindo o maior número de um array de números inteiros

---
+++ Atividade5.cs
@@ -0,0 +1,6 @@
+/*
+int[] numerosInteiros = { 1, 2, 3, 4, 5 };
+
+Console.WriteLine($"Números inteiros: {string.Join(", ", numerosInteiros)}");
+Console.WriteLine($"Maior numero: {numerosInteiros.Max()}");
+*/
