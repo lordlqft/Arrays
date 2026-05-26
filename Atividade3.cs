68e0303a9578ff957a700bffb1a1b4e6fe0292b6
Kenneth <lordlqft0@gmail.com>
2026-05-26 13:03:55 -0300

    Feat: Resolução da atividade 3
    Adaptar o código para resolução da atividade proposta no Notion

--- Atividade3.cs
+++ Atividade3.cs
@@ -1,6 +1,24 @@
-/*
-int[] numerosInteiros = { 1, 2, 3, 4, 5 };
+/* Crie um array para 5 números inteiros (digitados pelo usuário) e calcule a soma de todos os
+valores. */
 
-int soma = numerosInteiros.Sum();
+
+/*
+int[] numeros = new int[6];
+
+for (int i = 0; i < 6; i++)
+{
+    Console.Write($"Digite o {i + 1}º número: ");
+    numeros[i] = int.Parse(Console.ReadLine()!);
+}
+
+Console.WriteLine();
+Console.WriteLine("Soma de todos os números do array:");
+int soma = 0;
+
+for (int i = 0; i < 6; i++)
+{
+    soma += numeros[i];
+}
+
 Console.WriteLine(soma);
 */
