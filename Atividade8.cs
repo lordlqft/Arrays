415395adbacc8409eaccf855c8ec37b0db609aea
Kenneth <lordlqft0@gmail.com>
2026-05-28 08:07:08 -0300

    Delete Atividade8.cs

--- Atividade8.cs
+++
@@ -1,9 +0,0 @@
-/*
-string[] nomes = { "Kenneth", "Pedro", "João", "Rafaela", "Mariana" };
-
-Console.WriteLine("Nomes dos alunos de trás para frente:");
-for (int i = nomes.Length - 1; i >= 0; i--)
-{
-    Console.WriteLine(nomes[i]);
-}
-*/
